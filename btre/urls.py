0afa94655d5a63038b43f03045ba3bedc7484586
Joao Coelho <joaonc@hotmail.com>
2022-12-07 17:48:14 -0600

    urls and settings update

--- btre/urls.py
+++ btre/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]
