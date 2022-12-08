ee127e30ac3c8b79b9e023131565360323e417fa
Joao Coelho <joaonc@users.noreply.github.com>
2022-12-08 11:51:09 -0600

    Admin display data customization (#11)

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 
 from .models import Realtor
 
-admin.site.register(Realtor)
+
+# See ListingAdmin for more info on what each setting does
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'email',
+        'hire_date',
+    )
+    list_display_links = (
+        'id',
+        'name',
+    )
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+admin.site.register(Realtor, RealtorAdmin)
