185a854aefe08b6b56f76b15af29aa83a3963ca1
Joao Coelho <joaonc@hotmail.com>
2022-12-08 11:45:14 -0600

    listings customization

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,45 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    # Which properties from the listing to display as columns
+    list_display = (
+        'id',
+        'title',
+        'price',
+        'list_date',
+        'realtor',
+        'is_published',
+    )
+
+    # Which properties are clickable to edit
+    list_display_links = (
+        'id',
+        'title',
+    )
+
+    # Which properties can filter by
+    list_filter = (
+        'realtor',
+        'is_published',
+    )
+
+    # Which properties are editable
+    list_editable = ('is_published',)
+
+    # Which properties are searchable
+    search_fields = (
+        'title',
+        'description',
+        'address',
+        'city',
+        'state',
+        'price',
+    )
+
+    # Pagination
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
