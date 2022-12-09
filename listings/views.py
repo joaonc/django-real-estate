b9b97b68d54c1d41818f75084342964a3d50f7c7
Joao Coelho <joaonc@users.noreply.github.com>
2022-12-08 18:50:21 -0600

    Dynamic listings page (#12)

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+from .models import Listing
 
 
 def listings(request):
-    return render(request, 'listings/listings.html')
+    listings_ = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings_, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings,
+    }
+
+    return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id: int):
