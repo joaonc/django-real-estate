f3b1c126378cee1f4b529959fac33cc63124e086
Joao Coelho <joaonc@hotmail.com>
2022-12-08 18:34:05 -0600

    pagination prev/next

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,17 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .models import Listing
 
 
 def listings(request):
+    listings_ = Listing.objects.all()
+    paginator = Paginator(listings_, 2)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
     context = {
-        'listings': Listing.objects.all()
+        'listings': paged_listings,
     }
 
     return render(request, 'listings/listings.html', context)
