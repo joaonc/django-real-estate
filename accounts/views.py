ebbd66e58aad5e5e669986c4c5191a3ec3cda404
Joao Coelho <joaonc@users.noreply.github.com>
2022-12-10 16:12:00 -0600

    User registration (#19)

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.contrib import messages
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
 
@@ -17,11 +18,48 @@ def logout(request):
 
 def register(request):
     if request.method == 'POST':
-        # Register User
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name'].strip()
+        last_name = request.POST['last_name'].strip()
+        username = request.POST['username'].strip()
+        email = request.POST['email'].strip()
+        password = request.POST['password']
+        password2 = request.POST['password2']
 
-    return render(request, 'accounts/register.html')
+        # Check user doesn't exist
+        if User.objects.filter(username__iexact=username).exists():
+            messages.error(request, 'Username already exists')
+
+        # Check email doesn't exist
+        if User.objects.filter(email__iexact=email).exists():
+            messages.error(request, 'Email already exists')
+
+        # Check passwords match
+        if password != password2:
+            messages.error(request, 'Passwords do not match')
+
+        if messages.get_messages(request):
+            return redirect('register')
+
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+            password=password,
+        )
+        # Login after register
+        # auth.login(request, user)
+        # messages.success(request, 'You are now logged in')
+        # return redirect('index')
+
+        # Have user login after registration
+        user.save()
+        messages.success(request, 'You are now registered and can log in')
+        return redirect('login')
+
+    else:
+        return render(request, 'accounts/register.html')
 
 
 def dashboard(request):
