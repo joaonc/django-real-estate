13073acd52c40328f6dd43cf97f03f5a566ef083
Joao Coelho <joaonc@hotmail.com>
2022-12-09 23:05:31 -0600

    register accounts urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return redirect('index')
+
+
+def register(request):
+    return render(request, 'accounts/register.html')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
